62737d9c29cd0790fc6ee05287c554a1c14b5139
miladesoja <lizzwurzel@gmail.com>
2024-05-17 08:28:39 -0300

    pitoo

--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@
 #   2- Correr en la terminal "pytest tests.py"
 
 import pytest
-from matricesRalas import MatrizRala
+from matrices_ralas import MatrizRala, GaussJordan
 import numpy as np
 
 class TestIndexacionMatrices:
@@ -89,7 +89,7 @@ class TestProductoMatricial:
 
         C = A @ B
 
-        assert C.shape[0] == 2 and C.shape[1]==3 and C[0,0] == 9 and all( [C[i,i] == 0 for i in range(3) for j in range(4) if (i!=j and i!=0)] )
+        assert C.shape[0] == 2 and C.shape[1]==3 and C[0,0] == 9 and C[0,1] == 0 and C[0,2] == 0 and C[1,0] == 12 and C[1,1] == 0 and C[1,2] == 0
 
     def test_productoPorIdentidad( self ):
         A = MatrizRala(3,3)
@@ -106,3 +106,83 @@ class TestProductoMatricial:
         C1 = A @ Id
         C2 = Id @ A
         assert C1[0,0] == 1 and C1[0,2] == 3 and C1[1,2] == 4 and C2[0,0] == 1 and C2[0,2] == 3 and C2[1,2] == 4 and C1.shape == C2.shape and C1.shape == A.shape
+
+class TestGaussJordan:
+    def test_tamanos( self ):
+        A = MatrizRala(2,2)
+        b = MatrizRala(3,1)
+
+        with pytest.raises(Exception) as e_info:
+            GaussJordan(A,b)
+
+    def test_matrizSingularTiraError( self ):
+        A = MatrizRala(3,3)
+        b = MatrizRala(3,1)
+
+        b[0,0] = 1
+        b[1,0] = 2
+        b[2,0] = 3
+
+        with pytest.raises(Exception) as e_info:
+            GaussJordan(A,b)
+
+    def test_identidad( self ):
+        A = MatrizRala(3,3)
+        b = MatrizRala(3,1)
+
+        A[0,0] = 1
+        A[1,1] = 1
+        A[2,2] = 1
+
+        b[0,0] = 1
+        b[1,0] = 2
+        b[2,0] = 3
+
+        x = GaussJordan(A,b)
+
+        assert x[0,0] == 1 and x[1,0] == 2 and x[2,0] == 3
+
+    def test_triangularSup( self ):
+        A = MatrizRala(3,3)
+        b = MatrizRala(3,1)
+
+        A[0,0] = 1
+        A[1,1] = 1
+        A[2,2] = 1
+        A[0,1] = 1
+        A[0,2] = 1
+        A[1,2] = 1
+
+        b[0,0] = 1
+        b[1,0] = 2
+        b[2,0] = 3
+
+        x = GaussJordan(A,b)
+
+        assert np.isclose( x[0,0], -1 ) and np.isclose(x[1,0], -1) and np.isclose(x[2,0], 3)
+
+    def test_completa( self ):
+        A = MatrizRala(3,3)
+        b = MatrizRala(3,1)
+
+        A[0,0] = 1
+        A[1,0] = 2
+        A[2,0] = 3
+
+        A[2,1] = 5
+        A[0,2] = 12
+        A[2,2] = 2.34
+
+        b[0,0] = 1
+        b[1,0] = 1
+        b[2,0] = 1
+
+        x = GaussJordan(A,b)
+
+        assert np.isclose( x[0,0], 0.5 ) and np.isclose(x[1,0], -0.1195) and np.isclose(x[2,0], 0.041667)
+
+
+
+
+
+
